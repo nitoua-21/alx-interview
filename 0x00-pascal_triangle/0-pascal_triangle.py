b48c06d317b44136496f5b30daf3df170ec5eaf4
Normich ITOUA <nitoua.dev@gmail.com>
2024-10-03 09:06:56 -0700

    Fix Pycodestyle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,6 +2,8 @@
 """
 Pascal's Triangle
 """
+
+
 def pascal_triangle(n):
     """
     Generate Pascal's Triangle up to the nth row using a recursive approach.
@@ -15,17 +17,17 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    
+
     triangle = [[1]]
-    
+
     for i in range(1, n):
         prev_row = triangle[-1]
         new_row = [1]
-        
+
         for j in range(1, i):
             new_row.append(prev_row[j-1] + prev_row[j])
-        
+
         new_row.append(1)
         triangle.append(new_row)
-    
+
     return triangle
