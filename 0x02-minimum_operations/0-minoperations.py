78243bcc771737a135570d58ab2c0e0cdf7e4a74
Normich ITOUA <nitoua.dev@gmail.com>
2024-10-20 06:41:09 -0700

    Initial Commit

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Module that contains the minOperations function.
+"""
+
+
+def minOperations(n):
+    """
+    Calculates the fewest number of operations needed
+    to result in exactly n H characters in the file.
+
+    Args:
+    n (int): The target number of H characters.
+
+    Returns:
+    int: The minimum number of operations.
+    Returns 0 if n is impossible to achieve.
+    """
+    if n <= 1:
+        return 0
+
+    operations = 0
+    factor = 2
+
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
+
+    return operations
